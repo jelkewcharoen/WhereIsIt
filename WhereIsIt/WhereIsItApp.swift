fcb28ec12a937e93d113804f63e9772fbb7c423a
alukman3 <alukman3@gatech.edu>
2021-10-07 20:57:02 -0400

    Initial project intialization

---
+++ WhereIsIt/WhereIsItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsItApp.swift
+//  WhereIsIt
+//
+//  Created by Andrew Lukman on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

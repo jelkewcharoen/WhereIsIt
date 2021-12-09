22f9a2a79ce4c81d908037e974e6ec1883e09b18
Lukman, Andrew Widjaja <alukman3@gatech.edu>
2021-12-09 18:05:56 -0500

    Fix MapView preview errors to allow for Map preview when coding

--- WhereIsIt/Views/MapView.swift
+++ WhereIsIt/Views/MapView.swift
@@ -60,7 +60,8 @@ struct MapView: UIViewRepresentable {
 }
 
 struct MapView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        MapView(selectedEntity: .constant("testing"))
     }
 }
